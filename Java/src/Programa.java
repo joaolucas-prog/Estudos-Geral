e4d26657bb49c385863e505397b5ed9577514dfe
João Lucas <joaolucasxd012@gmail.com>
2020-10-30 09:25:43 -0300

    estudando java e linux

---
+++ Java/src/Programa.java
@@ -0,0 +1,9 @@
+/* Código mais simples possível no JAVA , ele apenas imprime Hello World na tela do console*/
+public class Programa {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+
+}

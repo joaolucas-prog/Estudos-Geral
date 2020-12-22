2aa0120fe8da40ac80f73993f62f429d5cb244fb
João Lucas <joaolucasxd012@gmail.com>
2020-12-22 20:30:10 -0300

    estudos diversos

---
+++ Ruby on Rails/1. logica de programação/aula3.rb
@@ -0,0 +1,24 @@
+#case when else
+numero = 3 
+case numero
+when  1
+    puts "1"
+when 2
+    puts "2"
+when 3
+    puts "3"
+else
+    puts "else"
+end
+#variáveis locais são acessíveis dentro da propria função
+
+#while
+i = 0
+while i < 3
+    puts i
+    i++
+end 
+
+#loop do 
+#um loop que roda eternamente
+#usar interrogação para dize que retorna booleano
--- Ruby on Rails/1. logica de programação/maior_ou_menor.rb
+++ Ruby on Rails/1. logica de programação/maior_ou_menor.rb
@@ -4,11 +4,24 @@ def da_boas_vindas
     nome = gets.strip #criando variavel nome e fazendo um input e retirando do inicio e final espaços e quebra de linha
     puts "Começaremos o jogo para você, #{nome}" # interpolando string para não usar nome.to_s por exemplo
     puts "\n\n\n\n"
+    nome
 end
 
-def sorteia_numero_secreto
-    puts "Escolhendo um número secreto entro 0 e 300"
-    sorteado = 150 
+def sorteia_numero_secreto (dificuldade)
+    case dificuldade
+    when 1
+        maximo = 30
+    when 2
+        maximo = 60
+    when 3
+        maximo = 100
+    when 4
+        maximo = 150
+    else
+        maximo = 200
+    end
+    puts "Escolhendo um número secreto entre 1 e #{maximo}"
+    sorteado = rand(maximo) + 1
     puts "Escolhido.. que tal adivinhar o número"
     sorteado# no ruby toda função sempre retorna algo , e esse algo é a ultima linha e não precisa do nome return
 end
@@ -39,20 +52,42 @@ def verifica_se_acertou(numero_secreto,chute)
     false
 end
 
-pontos_ate_agora = 1000
-da_boas_vindas
-numero_secreto = sorteia_numero_secreto
-chutes = [] #array
-limite_de_tentativas = 5
+def pede_uma_dificuldade
+    puts "Escolha uma dificuldade entre 1 e 5 (1 fácil...5 difícil)"
+    dificuldade = gets.to_i
+    dificuldade
+end
 
-for tentativa in 1..limite_de_tentativas # for in
-    chute = pede_um_numero chutes,tentativa,limite_de_tentativas# programadores ruby não coloca ()
-    chutes << chute# adiciona na ultima posição de chutes o chute 
-    pontos_ate_agora = (chute - numero_secreto).abs / 2.0
-    pontos_ate_agora -= pontos_a_perder
-    if verifica_se_acertou numero_secreto,chute # poderia usar break if e retirar o break e end
+def joga nome,dificuldade
+    pontos_ate_agora = 1000
+    numero_secreto = sorteia_numero_secreto dificuldade
+    chutes = [] #array
+    limite_de_tentativas = 5
+
+    for tentativa in 1..limite_de_tentativas # for in
+        chute = pede_um_numero chutes,tentativa,limite_de_tentativas# programadores ruby não coloca ()
+        chutes << chute# adiciona na ultima posição de chutes o chute 
+        pontos_a_perder = (chute - numero_secreto).abs / 2.0
+        pontos_ate_agora -= pontos_a_perder
+        if verifica_se_acertou numero_secreto,chute # poderia usar break if e retirar o break e end
+            break
+        end
+    end
+    puts "Você ganhou #{pontos_ate_agora} pontos"
+end
+
+def nao_quer_jogar?
+    puts "Deseja jogar Novamente ?"
+    resposta = gets.strip
+    resposta.upcase == "N"
+end
+
+nome = da_boas_vindas
+dificuldade = pede_uma_dificuldade
+loop do
+    joga nome,dificuldade
+    if nao_quer_jogar?
         break
     end
 end
 
-puts "Você ganhou #{pontos_ate_agora} pontos"

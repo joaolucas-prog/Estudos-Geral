80eda2c099253a97edd177c77146af25a3f52533
João Lucas <joaolucasxd012@gmail.com>
2021-08-30 09:39:12 -0300

    estudando

--- Foguete/GSB11/backend/src/index.js
+++ Foguete/GSB11/backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors =  require("cors");
 const { v4: uuidv4, validate } = require("uuid");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const projects = [];
--- Foguete/GSB11/frontend/src/App.js
+++ Foguete/GSB11/frontend/src/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Header from './components/Header'
 import './App.css';
 import backgroundImage from './assets/background.jpg'
+import api from './services/api'
 
 export default function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento Web', 'Desenvolvimento mobile']);
+  const [projects, setProjects] = useState([]);
+  
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
 
-  function handleAddProject() {
-    setProjects([ ...projects, `Novo projeto : ${Date.now()}` ]);
-    console.log(projects);
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto : ${Date.now()}`,
+      owner: 'dev'
+    });
+    const project = response.data;
+    setProjects([...projects, project]);
   }
 
   return (
@@ -18,7 +29,7 @@ export default function App() {
       <img width={300} src={backgroundImage}/>
         <ul>
           {projects.map(project => (
-            <li key={project}>{project}</li>)
+            <li key={project.id}>{project.title}</li>)
           )}
         </ul>
         <button type="button" onClick={handleAddProject}>Adicionar novo projeto</button>
